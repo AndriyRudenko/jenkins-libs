1814f07b4e177750543d7feacafc11f6b4d9031a
AndriyRudenko <zmeygorunuch87@gmail.com>
2021-07-02 23:49:06 +0300

    Update lib.groovy

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -1,4 +1,4 @@
-@Grab(group='groovy', module='yaml', version='3.0.0')
+@Grab(group='java.lang.Object', module='groovy.lang.GroovyObjectSupport', version='3.0.0')
 import groovy.yaml.YamlBuilder
 
 def test() {
