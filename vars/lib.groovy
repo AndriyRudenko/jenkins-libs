b054db7a0444c46c08fc870d15fbc9e702801141
AndriyRudenko <zmeygorunuch87@gmail.com>
2021-07-02 22:28:56 +0300

    Create lib.groovy

---
+++ vars/lib.groovy
@@ -0,0 +1,20 @@
+import groovy.yaml.YamlBuilder
+def test {
+
+def yamlBuilder = new YamlBuilder()
+yamlBuilder{
+    car{
+        name 'Volvo'
+        make 'Kyiv'
+        other{
+            param1 'param1'
+            param2 'param2'
+        }
+    }
+}
+
+File filename1 = new File ("out1.yaml")
+filename1.write(yamlBuilder.toString())
+println(filename1)
+
+}
